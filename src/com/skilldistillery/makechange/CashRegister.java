88edf782e9c6f9f1e1c74bec56a10db5a0db8502
Adam Tappy <adamtappy@Brucebs-MacBook-Pro-2.local>
2019-01-11 22:57:00 -0700

    Finished the basic functionality.  Still TODO: 1) Print only the bills/coins used to make change.  2) Review calcBills() & calcCoins() logic.

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -3,32 +3,94 @@ package com.skilldistillery.makechange;
 public class CashRegister {
     private double purchasePrice;
     private double cashTendered;
-    
-    CashRegister(double price, double cash){
+    private byte twenty, ten, five, one, quarter, dime, nickel, penny;
+    private double changeDue;
+
+    CashRegister(double price, double cash) {
 	this.purchasePrice = price;
 	this.cashTendered = cash;
+	this.changeDue = this.cashTendered - this.purchasePrice;
     }
-    
+
     public String makeChange() {
-	if (this.cashTendered < this.purchasePrice) {
-	    return "Insufficient funds.";
-	}
-	else if(this.purchasePrice <= 0) {
-	    return "Invalid purchase price.";
-	}
-	else {
-	    
+	if (this.changeDue < 0) {
+	    System.out.println("Insufficient funds.");
 	    return "";
+	} else if (this.purchasePrice <= 0) {
+	    System.out.println("Invalid purchase price.");
+	    return "";
+	} 
+	if (this.changeDue >= 1.0) {
+	    calcBills();
+	}
+	if (this.changeDue >= .01) {
+	    calcCoins();
+	}
+
+	return getChangeSummary();
+    }
+
+    private void calcBills() {
+	int changeDueRemaining = (int) this.changeDue;
+	int temp;
+	
+	if (changeDueRemaining >= 20) {
+	    temp = (byte) (changeDueRemaining / 20);
+	    this.twenty += temp;
+	    changeDueRemaining -= temp * 20;
+	}
+	if (changeDueRemaining >= 10) {
+	    temp = (byte) (changeDueRemaining / 10);
+	    this.ten += temp;
+	    changeDueRemaining -= temp * 10;
+	}
+	if (changeDueRemaining >= 5) {
+	    temp = (byte) (changeDueRemaining / 5);
+	    this.five += temp;
+	    changeDueRemaining -= temp * 5;
+	}
+	if (changeDueRemaining >= 1) {
+	    temp = (byte) (changeDueRemaining / 1);
+	    this.one += temp;
+	    changeDueRemaining -= temp;
 	}
     }
-    
-    public static String calcBills() {
-	
-	return "";
+
+    private void calcCoins() {
+	int changeDueRemaining = (int)((this.changeDue - (int)this.changeDue) * 100);
+	double temp;
+
+	if (changeDueRemaining >= 25) {
+	    temp = changeDueRemaining / 25;
+	    this.quarter += temp;
+	    changeDueRemaining -= temp * 25;
+	}
+	if (changeDueRemaining >= 10) {
+	    temp = changeDueRemaining / 10;
+	    this.dime += temp;
+	    changeDueRemaining -= temp * 10;
+	}
+	if (changeDueRemaining >= 5) {
+	    temp = changeDueRemaining / 5;
+	    this.nickel += temp;
+	    changeDueRemaining -= temp * 5;
+	}
+	if (changeDueRemaining >= 1) {
+	    temp = changeDueRemaining / 1;
+	    this.penny += temp;
+	    changeDueRemaining -= temp;
+	}
     }
-    
-    public static String calcCoins() {
+
+    public String getChangeSummary() {
+	String change = "";
 	
-	return "";
+	change += "\nChange due to customer: $" + this.changeDue;
+	change += "\n\tTwenties: " + this.twenty + "\n\tTens: " + this.ten + "\n\tFives: " + this.five + "\n\tOnes: "
+		+ this.one;
+	change += "\n\tQuarters: " + this.quarter + "\n\tDimes: " + this.dime + "\n\tNickels: " + this.nickel
+		+ "\n\tPennies: " + this.penny;
+
+	return change;
     }
 }
